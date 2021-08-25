bef754ba65cef7f801daefab7a9281527814c277
merdeevrz <merdeevrz@haulmont.com>
2021-08-25 15:15:48 +0400

    Initial commit

---
+++ src/test/java/com/example/spring_luxoft_boot/SpringLuxoftBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_luxoft_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLuxoftBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
